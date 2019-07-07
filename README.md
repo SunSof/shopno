87e9789a429d57d547c729bdae921cfac7a68880
SunSof <51479944+SunSof@users.noreply.github.com>
2019-07-07 20:35:26 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# shopno
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.scss";
+import "./hamburger.scss";
+import "./hello.scss";
+import "./about.scss";
+import "./services.scss";
+import "./services-card.scss";
+import "./team-card.scss";
+import "./say.scss";
+import "./contacts.scss";
+import "./follow.scss";
+import "./say-slider.scss";
