3944f6e0322d23442d1de295215b7b9f28896e32
SunSof <buketovasofi@gmail.com>
2019-07-28 19:56:26 +0300

    made a window on click

--- src/index.js
+++ src/index.js
@@ -10,3 +10,15 @@ import "./contacts.scss";
 import "./follow.scss";
 import "./say-slider.scss";
 import "./mixins.scss";
+var banner = $(".b-card--bg1-hov")
+var button = $("button")
+
+// handle click and add class
+$(".js-open").on("click", function(){
+  banner.addClass("alt")
+})
+
+// handle click and remove class
+$(".js-close").on("click", function(){
+  banner.removeClass("alt")
+})
